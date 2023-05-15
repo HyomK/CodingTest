b4105cf77c6e961368d6c0ad7af9d6d9da822894
Hyomk <qet557@gmail.com>
2023-05-15 22:51:25 +0900

    do : 2023-05-14 #30

--- src/main/kotlin/BOJ/math/P2960.kt
+++ src/main/kotlin/BOJ/math/P2960.kt
@@ -1,4 +1,44 @@
 package BOJ.math
+import java.util.*
+import java.io.*
+
+fun main(){
+    P2960().solution()
+}
 
 class P2960 {
+    fun solution()=with(System.`in`.bufferedReader()){
+        val br = readLine().split(" ")
+        val N = br[0].toInt()
+        val K = br[1].toInt()
+
+
+        val checker = BooleanArray(N+1){false}
+        checker[0] = true
+
+        var cnt = 0
+        for(i in 2 ..N){
+            if(!checker[i] && isPrime(i)){
+               //배수를 제거한다
+                for(j in i ..N step i){
+                    if(!checker[j]){
+                        checker[j] = true
+                        cnt ++
+                    }
+                    if(cnt == K){
+                        println(j)
+                        return
+                    }
+                }
+            }
+        }
+
+    }
+
+    fun isPrime(n : Int) : Boolean{
+        for(i in 2 until n){
+            if(n%i==0) return false
+        }
+        return true
+    }
 }
